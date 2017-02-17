e801c91ee7883b21ec70b8df20e86ebeba19d7a6
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-02-17 18:00:03 -0200

    Removing console.log from google-analytics.

--- src/js/model/google-analytics/google-analytics.js
+++ src/js/model/google-analytics/google-analytics.js
@@ -1,5 +1,3 @@
-console.log("GoogleAnalyticsObject");
-
 (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
 function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
 e=o.createElement(i);r=o.getElementsByTagName(i)[0];
