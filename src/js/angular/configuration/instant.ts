1c9bf59d25d4d713f1e2c555a64d7519aea4a89a
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-02-17 17:42:26 -0200

    Creating instant class file. Helper to quickly changes the default configuration.

---
+++ src/js/angular/configuration/instant.ts
@@ -0,0 +1,3 @@
+export const instant = {
+  test: true
+};
--- src/js/angular/main.ts
+++ src/js/angular/main.ts
@@ -3,6 +3,7 @@ import './configuration/dependencies';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './../environments/default';
+import { instant } from './configuration/instant';
 
 import { AppModule } from './components/app/app.module';
 import { TestModule } from './components/test/test.module';
@@ -11,4 +12,8 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(TestModule);
+if (!instant.test) {
+  platformBrowserDynamic().bootstrapModule(AppModule);
+} else {
+  platformBrowserDynamic().bootstrapModule(TestModule);
+}
