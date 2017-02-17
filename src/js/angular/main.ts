eee3021e9f03237164124a83a97f1059512c0312
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-02-17 17:32:32 -0200

    Adding dependencies file, configuring tsconfig.json and moving google-analytics.

--- src/js/angular/main.ts
+++ src/js/angular/main.ts
@@ -1,6 +1,9 @@
+import './configuration/dependencies';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './../environments/default';
+
 import { AppModule } from './components/app/app.module';
 import { TestModule } from './components/test/test.module';
 
