aa367aecd9ef7d3a553bf485745ada413306558e
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-02-20 10:07:44 -0300

    Little improvements and cleaning.

--- src/js/angular/main.ts
+++ src/js/angular/main.ts
@@ -3,17 +3,11 @@ import './configuration/dependencies';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './../environments/default';
-import { instant } from './configuration/instant';
 
 import { AppModule } from './components/app/app.module';
-import { TestModule } from './components/test/test.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-if (!instant.test) {
-  platformBrowserDynamic().bootstrapModule(AppModule);
-} else {
-  platformBrowserDynamic().bootstrapModule(TestModule);
-}
+platformBrowserDynamic().bootstrapModule(AppModule);
