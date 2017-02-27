4aa8c9f21b9c89a87dc11bbffad84a6326ab718e
Danilo Araújo Silva <silva.danilo.araujo@gmail.com>
2017-02-27 15:11:25 -0300

    Stable integration with a loading page.

--- src/js/main/angular/component/app/app.module.ts
+++ src/js/main/angular/component/app/app.module.ts
@@ -38,23 +38,23 @@ import { PublicModule } from './../public/public.module';
 	]
 })
 export class AppModule {
-	loading: boolean = true;
-
-	constructor(private router: Router) {
-		router.events.subscribe((event: RouterEvent) => {
-			this.navigationInterceptor(event);
-		});
-	}
-
-	navigationInterceptor(event: RouterEvent): void {
-		if (event instanceof NavigationStart) {
-			this.loading = true;
-		} else if (event instanceof NavigationEnd) {
-			setTimeout(() => this.loading = false, 1000);
-		} else if (event instanceof NavigationCancel) {
-			this.loading = false;
-		} else if (event instanceof NavigationError) {
-			this.loading = false;
-		}
-	}
+	// loading: boolean = true;
+	//
+	// constructor(private router: Router) {
+	// 	router.events.subscribe((event: RouterEvent) => {
+	// 		this.navigationInterceptor(event);
+	// 	});
+	// }
+	//
+	// navigationInterceptor(event: RouterEvent): void {
+	// 	if (event instanceof NavigationStart) {
+	// 		this.loading = true;
+	// 	} else if (event instanceof NavigationEnd) {
+	// 		setTimeout(() => this.loading = false, 1000);
+	// 	} else if (event instanceof NavigationCancel) {
+	// 		this.loading = false;
+	// 	} else if (event instanceof NavigationError) {
+	// 		this.loading = false;
+	// 	}
+	// }
 }
